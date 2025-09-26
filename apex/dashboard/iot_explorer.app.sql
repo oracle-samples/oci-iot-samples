deb72a8b08d1a2f65e335c32219aa2a5e314684b
Micheal Simmons <micheal.simmons@oracle.com>
2025-09-26 09:15:23 -0400

    feat: :sparkles: Added command data monitoring, IoT domain information, and OCI settings information
    Command Data Monitoring: This feature is now integrated throughout the application, providing real-time visibility and insights into command-related activities. Domain Information Visibility: Easily access domain group and domain details for the environment where the app is installed. Settings Page: A dedicated page is now available for entering OCI credentials and other essential OCI configuration details. This setup is required to unlock new and upcoming features in the OCI IoT Explorer.
    
    Signed-off-by: Micheal Simmons <micheal.simmons@oracle.com>

---
+++ apex/dashboard/iot_explorer.app.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:01f17640ee309ba3dc201fb262fb4fcd4a4ec11edfbf17dcc08f9626ce84d7b4
+size 844901
---
+++ apex/dashboard/iot_explorer_teardown.sql
@@ -0,0 +1,65 @@
+--
+-- This script grants supporting objects for the IoT Platform Explorer.
+--
+-- Copyright (c) 2025 Oracle and/or its affiliates.
+-- Licensed under the Universal Permissive License v 1.0 as shown at
+-- https://oss.oracle.com/licenses/upl.
+--
+-- DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+--
+
+drop package IOT_INFO;
+drop package IOT_APEX;
+drop table IOT_CONFIG;
+drop view iot_hierarchy;
+drop view iot_stats;
+declare
+  l_job_exists number;
+  begin
+    select  count(*) into l_job_exists
+    from    user_scheduler_jobs
+    where   job_name = 'REFRESH_MVIEW_IOT_MSG_STATS';
+
+    if l_job_exists = 1 then
+      dbms_scheduler.drop_job(job_name => 'REFRESH_MVIEW_IOT_MSG_STATS');
+    end if;
+end;
+/
+declare
+    count_mv number;
+  begin
+    select count(*) into count_mv
+    from all_mviews
+    where mview_name = 'IOT_MSG_STATS';
+
+    if count_mv > 0 then
+        execute immediate 'drop materialized view iot_msg_stats';
+    end if;
+end;
+/
+declare
+  cursor rec_cur is
+    select  'drop synonym '||lower(replace(view_name,'DIGITAL_TWIN_',''))||'_syn' as statement
+    from    all_views
+    where   upper(owner) = replace( sys_context( 'userenv', 'current_schema' ), '__WKSP', '__IOT' )
+    and     view_name not like 'AQ$%';
+  begin
+
+  for rec in rec_cur loop
+
+    begin
+
+      execute immediate rec.statement;
+      DBMS_OUTPUT.PUT_LINE('Successfully executed: ' || rec.statement);
+
+    exception
+      when others then
+        DBMS_OUTPUT.PUT_LINE('Failed to execute: ' || rec.statement || '. Error: ' || SQLERRM);
+
+    end;
+
+  end loop;
+
+end;
+/
+drop function compact_number;

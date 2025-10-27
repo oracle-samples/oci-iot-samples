3eb6b47bc7dd162717a5ee3a92de094ee363d01b
Micheal Simmons <micheal.simmons@oracle.com>
2025-10-27 10:14:22 -0400

    fix: :bug: Bug fixe for instance detail chart and buttons in IoT object reports
    Fixed the query powering the chart on the instance detail page. Some of the buttons inbeded in the report on the model, adapter, and instance pages would not work if the report was resorted.
    
    Signed-off-by: Micheal Simmons <micheal.simmons@oracle.com>

--- apex/dashboard/iot_explorer.app.sql
+++ apex/dashboard/iot_explorer.app.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52e7946a4ab55536826c70a64ecb3f0b0a2768a96637f9936d2e262a4dda3989
-size 1331661
+oid sha256:f7c02c2d55faaca431ca582a6604da21ec413419e2ee181437be7335a00c5f96
+size 1334491

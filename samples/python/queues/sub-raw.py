9633e4a0d4c61dd565260f9a27fdc8e2c8427b59
Philippe Vanhaesendonck <AmedeeBulle@users.noreply.github.com>
2025-11-10 20:07:42 +0100

    Update samples/python/queues/sub-raw.py
    Co-authored-by: Sergio Leunissen <sergio.leunissen@oracle.com>

--- samples/python/queues/sub-raw.py
+++ samples/python/queues/sub-raw.py
@@ -234,7 +234,7 @@ def unsubscribe(
 
     Args:
         connection (oracledb.Connection): The database connection.
-        queue_name (str): the name a the queue to stream from.
+        queue_name (str): the name of the queue to stream from.
         subscriber (oracledb.DbObject): A subscriber agent database object.
     """
     try:

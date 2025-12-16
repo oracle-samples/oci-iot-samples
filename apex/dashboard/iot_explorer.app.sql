a0fe353126ff00c402d57bb2b978431a34fa8395
Micheal Simmons <micheal.simmons@oracle.com>
2025-12-16 14:21:23 -0500

    fix: :bug: Changed / Fixed report order, version number, spelling
    Changed model, adapter, and instance report order to reflect OCI Console design. Implemented version number at the bottom of pages. Fixed spelling errors.
    
    Signed-off-by: Micheal Simmons <micheal.simmons@oracle.com>

--- apex/dashboard/iot_explorer.app.sql
+++ apex/dashboard/iot_explorer.app.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4b446138544a8db3235daf85254a524cdea78ba598b2cb6fc4fd480a94573cf
-size 1364653
+oid sha256:98cdbbb3c5833270d5939f5d2e08e54a581c1b7e2c710a0169d78cd88115662f
+size 1371521

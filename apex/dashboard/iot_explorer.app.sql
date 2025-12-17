1902887a3cd5d237d56e0354148365bfd2d4b4d8
Micheal Simmons <micheal.simmons@oracle.com>
2025-12-17 15:43:58 -0500

    style: :lipstick: Grammar, documentation, and style changes
    Added missing steps and the required git-lfs extention to the README file. Cleaned up page styling and gramatical errors in the application.  Added a condition to only show instance authentications if the proper settings were set.
    
    Signed-off-by: Micheal Simmons <micheal.simmons@oracle.com>

--- apex/dashboard/iot_explorer.app.sql
+++ apex/dashboard/iot_explorer.app.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98cdbbb3c5833270d5939f5d2e08e54a581c1b7e2c710a0169d78cd88115662f
-size 1371521
+oid sha256:31f5ad5a321cc04faae1ce78396dfb39e19d3671d61338703f538f162e249e3b
+size 1373379
